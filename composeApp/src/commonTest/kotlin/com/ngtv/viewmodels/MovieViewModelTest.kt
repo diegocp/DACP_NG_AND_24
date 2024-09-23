a11e28d3c482a697227da0b70d982eb33e531092
diegocp <diegocp@gmail.com>
2024-09-23 15:35:50 +0100

    update test unit

--- composeApp/src/commonTest/kotlin/com/ngtv/viewmodels/MovieViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/com/ngtv/viewmodels/MovieViewModelTest.kt
@@ -2,12 +2,13 @@ package com.ngtv.viewmodels
 
 import app.cash.molecule.RecompositionMode
 import app.cash.molecule.moleculeFlow
-import app.cash.turbine.Turbine
 import app.cash.turbine.test
+import app.cash.turbine.turbineScope
 import com.ngtv.data.domain.Genre
 import com.ngtv.data.domain.Movies
 import com.ngtv.usecases.MovieHomeData
 import com.ngtv.usecases.MovieUseCaseContract
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.test.runTest
@@ -34,15 +35,46 @@ class MovieViewModelTest {
 
   }
 
+  @Test
+  fun `on launch news screen should show display data`() = runTest {
+    turbineScope {
+      val useCase = FakeMovieUseCase()
+      val events = MutableSharedFlow<MovieUiEvents>(extraBufferCapacity = 20)
+      moleculeFlow(mode = RecompositionMode.Immediate) {
+        MoviePresenter(
+          events,
+          useCase
+        )
+      }.distinctUntilChanged().test {
+        events.emit(MovieUiEvents.LoadMovies)
+        awaitItem()
+        assertEquals(
+          expected = MovieUiState.Success(MovieHomeData(
+            Movies(emptyList()),
+            Movies(emptyList()),
+            Movies(emptyList()),
+            Movies(emptyList())
+          )),
+          actual = awaitItem(),
+          message = "Should display data if available"
+        )
+        cancel()
+      }
+    }
+  }
+
+
 }
 
 class FakeMovieUseCase : MovieUseCaseContract {
-  override suspend fun fetchMovies(): MovieHomeData = Turbine<MovieHomeData>().awaitItem()
+  override suspend fun fetchMovies(): MovieHomeData = MovieHomeData(
+    Movies(emptyList()), Movies(emptyList()), Movies(emptyList()), Movies(emptyList())
+  )
 
-  override suspend fun searchMovies(query: String) = Turbine<Movies>().awaitItem()
+  override suspend fun searchMovies(query: String) = Movies(emptyList())
 
-  override suspend fun searchByGenre(idsGenre: String) = Turbine<Movies>().awaitItem()
+  override suspend fun searchByGenre(idsGenre: String) = Movies(emptyList())
 
-  override suspend fun fetchGenres(): List<Genre> = Turbine<List<Genre>>().awaitItem()
+  override suspend fun fetchGenres(): List<Genre> = emptyList()
 
 }
